fffb804bc0e18eb86aa0798536d32559db64f43d
Gokul Kannan <gokulskannan1993@gmail.com>
2025-05-05 14:23:21 +0100

    Refactor signUp function in api.js; rename response variable to res for consistency and add newlines for improved readability

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,12 +2,18 @@ import { axiosInstance } from './axios';
 
 
 export const signUp = async (signUpData) => {
-    const response = await axiosInstance.post('/auth/signup', signUpData);
-    return response.data;
+    const res = await axiosInstance.post('/auth/signup', signUpData);
+    return res.data;
 }
 
 
 export const getAuthUser = async () => {
     const res = await axiosInstance.get("/auth/me");
+    return res.data;
+}
+
+
+export const completeVerfication = async (userData) => {
+    const res = await axiosInstance.post("/auth/onboarding", userData);
     return res.data;
 }
