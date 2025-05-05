a5fd4b1817f82c06e8001e55bb00f0a859f7081f
Gokul Kannan <gokulskannan1993@gmail.com>
2025-05-05 09:14:39 +0100

    Add react-hot-toast for notifications; update App component to include toast buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Routes, Route } from 'react-router'
+import { Toaster, toast } from 'react-hot-toast'
+
+
 import HomePage from './pages/HomePage.jsx'
 import SignUpPage from './pages/SignUpPage.jsx'
 import LoginPage from './pages/LoginPage.jsx'
@@ -11,7 +14,11 @@ import OnboardingPage from './pages/OnboardingPage.jsx'
 
 const App = () => {
   return (
-    <div data-theme="night h-screen">
+    <div data-theme="night" className="h-screen flex flex-col items-center justify-center">
+      <div className="space-y-4 flex flex-col items-center">
+        <button className="btn btn-primary " onClick={() => toast.success("Success Toast")}>Success Toast</button>
+        <button className="btn btn-primary" onClick={() => toast.error("Error Toast")}>Error Toast</button>
+      </div>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUpPage />} />
@@ -21,7 +28,7 @@ const App = () => {
         <Route path="/chat" element={<ChatPage />} />
         <Route path="/onboarding" element={<OnboardingPage />} />
       </Routes>
-
+      <Toaster />
     </div>
   )
 }
