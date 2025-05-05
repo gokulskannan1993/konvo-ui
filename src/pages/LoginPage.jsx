754a89fb671c6556539e9074c8846654d17fddb7
Gokul Kannan <gokulskannan1993@gmail.com>
2025-05-05 19:50:11 +0100

    Enhance LoginPage; implement login functionality with error handling and improved UI layout

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,111 @@
-import React from 'react'
+import { React, useState } from 'react'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { toast } from 'react-hot-toast'
+import { LifeBuoy } from 'lucide-react'
+import { login } from '../lib/api'
+import { Link } from 'react-router'
 
 const LoginPage = () => {
+
+    const [loginData, setLoginData] = useState({
+        email: '',
+        password: ''
+    })
+
+
+    const queryClient = useQueryClient()
+    const { mutate: loginMutation, isPending, error } = useMutation({
+        mutationFn: login,
+        onSuccess: () => {
+            // Invalidate and refetch
+            queryClient.invalidateQueries({ queryKey: ['authUser'] })
+            toast.success('Login successful')
+        },
+        onError: (error) => {
+            toast.error(error.message)
+        }
+    })
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+        loginMutation(loginData)
+    }
+
+
+
+
     return (
-        <div>LoginPage</div>
+        <div className="flex items-center justify-center h-screen p-4 sm:p-6 md:p-8" data-theme="forest">
+            <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden">
+                {/* Left Side Form */}
+                <div className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col">
+                    <div className="mb-4 flex items-center justify-start gap-2">
+                        <LifeBuoy className="size-9 text-primary" />
+                        <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider">Konvo</span>
+                    </div>
+
+                    {error && (<div className="alert alert-error shadow-lg mb-4">
+                        <span>{error.response.data.message}</span>
+                    </div>)}
+
+                    <div className="w-full">
+                        <form onSubmit={handleLogin}>
+                            <div className="space-y-4">
+                                <h2 className="text-3xl font-semibold ">Welcome Back</h2>
+                                <p className="text-sm ">Sign in to your account to continue your language journey</p>
+
+
+                            </div>
+                            <div className="form-control w-full mb-4">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="Enter your email" className="input input-bordered w-full" required value={loginData.email} onChange={(e) => setLoginData({ ...loginData, email: e.target.value })} />
+                            </div>
+
+                            <div className="form-control w-full mb-4">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="Enter your password" className="input input-bordered w-full" required value={loginData.password} onChange={(e) => setLoginData({ ...loginData, password: e.target.value })} />
+                            </div>
+
+                            <div className="form-control w-full mb-4">
+                                <label className="label cursor-pointer justify-start gap-2">
+                                    <span className="label-text">Forgot Password?</span>
+                                    <Link to="/login" className="text-primary">Reset Password</Link>
+                                </label>
+                            </div>
+
+
+                            <button type='submit' className="btn btn-primary w-full" disabled={isPending}>
+                                {isPending ? <span className="loading loading-spinner loading-sm"></span> : 'Login'}
+                            </button>
+
+                        </form>
+                        <div className="divider my-4">OR</div>
+                        <div className="flex items-center justify-between mb-4">
+                            <p className="text-sm ">Don't have an account? <Link to="/signup" className="text-primary">Sign Up</Link></p>
+                        </div>
+
+                    </div>
+
+
+                </div>
+                {/* Right Side Image */}
+                <div className="hidden lg:flex lg:w-1/2 w-full bg-primary/10 p-4 sm:p-8 items-center justify-center">
+                    <div className="max-w-md p-8">
+                        <div className="relative aspect-square max-w-sm mx-auto">
+                            <img src="../public/i.png" alt="Sign Up" className="w-full h-full" />
+                        </div>
+                        <div className="text-center mt-6 space-y-3">
+                            <h2 className="text-xl font-semibold">Connect with language partners worldwide</h2>
+                            <p className="text-sm opacity-70">Join our community and start learning today!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
-
 export default LoginPage
