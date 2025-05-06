0d5d00508f9a3cef8e89526eb1cab76ee87d004f
Gokul Kannan <gokulskannan1993@gmail.com>
2025-05-06 19:21:58 +0100

    Integrate theme management into PageLoader component by utilizing useThemeStore

--- src/components/PageLoader.jsx
+++ src/components/PageLoader.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { LoaderCircle } from 'lucide-react'
+import { useThemeStore } from '../store/useThemeStore.js'
 
 const PageLoader = () => {
 
+    const { theme } = useThemeStore()
+
 
     return (
-        <div className='min-h-screen flex items-center justify-center'>
+        <div className='min-h-screen flex items-center justify-center' data-theme={theme}>
             <LoaderCircle className='animate-spin text-primary size-11' />
         </div>
     )
