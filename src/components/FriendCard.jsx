4d4552397d4a0a46ca50642d696a51862379b4ab
Gokul Kannan <gokulskannan1993@gmail.com>
2025-05-06 22:57:25 +0100

    Refactor FriendCard to improve layout and enhance language display with proper capitalization

--- src/components/FriendCard.jsx
+++ src/components/FriendCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { getLanguageFlag } from '../lib/utils.js'
+import { getLanguageFlag, capitalizeFirstLetter, formatLocation } from '../lib/utils.js'
 import { Link } from 'react-router'
 
 
@@ -12,16 +12,24 @@ const FriendCard = ({ friend }) => {
                         <img src={friend?.profilePicture} alt={friend?.name} />
 
                     </div>
-                    <h3 className='font-semibold truncate'>{friend?.name}</h3>
+                    <div className='flex flex-col justify-center'>
+                        <h3 className='font-semibold truncate'>{user?.name}</h3>
+                        {friend.country && (
+                            <div className='flex items-center text-xs opacity-70 mt-1'>
+                                <MapPin className='size-3 mr-1' />
+                                {formatLocation(friend?.country)}
+                            </div>
+                        )}
+                    </div>
                 </div>
                 <div className='flex flex-wrap gap-1.5 mb-3'>
                     <span className='badge badge-secondary text-xs'>
                         <img src={getLanguageFlag(friend?.nativeLanguage)} alt={friend?.nativeLanguage} className="inline-block h-3 mr-1" />
-                        Native: {friend?.nativeLanguage}
+                        Native: {capitalizeFirstLetter(friend?.nativeLanguage)}
                     </span>
-                    <span className='badge badge-secondary text-xs'>
+                    <span className='badge badge-primary text-xs'>
                         <img src={getLanguageFlag(friend?.learningLanguage)} alt={friend?.learningLanguage} className="inline-block h-3 mr-1" />
-                        Learning: {friend?.learningLanguage}
+                        Learning: {capitalizeFirstLetter(friend?.learningLanguage)}
                     </span>
                 </div>
                 <Link to={`/chat/${friend?._id}`} className='btn btn-outline  w-full'>Message</Link>
