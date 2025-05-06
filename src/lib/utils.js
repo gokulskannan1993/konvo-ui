c5f92122f9dde329c96dd09bd438ebfdd828e9ec
Gokul Kannan <gokulskannan1993@gmail.com>
2025-05-06 22:57:30 +0100

    Add formatLocation utility function and refactor capitalizeFirstLetter for improved string handling

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -10,3 +10,22 @@ export function getLanguageFlag(language) {
         return null; // or a default flag
     }
 }
+
+
+export function capitalizeFirstLetter(string) {
+    if (!string) return null;
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+export function formatLocation(location) {
+    return location
+        .split(', ') // Split by comma and space
+        .map(part => part.split(' ') // Split each part by spaces
+            .map(word => {
+                // If the word is an abbreviation (e.g., "US"), keep it uppercase
+                return word.length <= 2 ? word.toUpperCase() : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+            })
+            .join(' ') // Join words back together
+        )
+        .join(', '); // Join the parts back together with a comma and space
+}
