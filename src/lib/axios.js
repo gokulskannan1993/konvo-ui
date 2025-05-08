7ec88ab9030fdd09be721f9de74ede7657dba893
Gokul Kannan <gokulskannan1993@gmail.com>
2025-05-08 22:25:49 +0100

    refactor: simplify BASE_URL assignment in axios.js for cleaner code

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const BASE_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5001/api"
-    : "/api/"; // Replace with your production URL
+  import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
