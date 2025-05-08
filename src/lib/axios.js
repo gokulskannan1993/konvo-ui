0e6c8c30be9fc71c913ea88c866df7306629db98
Gokul Kannan <gokulskannan1993@gmail.com>
2025-05-08 22:16:14 +0100

    feat: update package.json and package-lock.json to include project-konvo dependency; refactor axios.js to use dynamic BASE_URL

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
-export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api",
-    withCredentials: true,
-});
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "/api/"; // Replace with your production URL
 
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+});
